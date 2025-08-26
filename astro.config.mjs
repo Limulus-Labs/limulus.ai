d822efc2811cdb53e9ab0b78036dae9518b07597
matashaEs <natali.babinska@ukr.net>
2025-08-26 17:15:45 +0200

    defineConfig

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-  site: 'https://matashaes.github.io/limulus',
+  site: 'https://limulus-labs.github.io/limulus.ai/',
   base: 'limulus',
 })
